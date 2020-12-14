097f3ee3b728ca34475ac9945489f1949b801aa1
dgh960527 <1165518623@qq.com>
2020-12-15 01:23:18 +0800

    推送反馈

--- ftp_client.py
+++ ftp_client.py
@@ -17,6 +17,8 @@ def ftpconnect(host, username, password):
     return ftp
 
 def downloadfile(ftp, remotepath, localpath, filename):
+    result = False
+
     ftp.cwd(remotepath)  # 设置FTP远程目录(路径)
     list = ftp.nlst()  # 获取目录下的文件,获得目录列表
     for name in list:
@@ -26,8 +28,10 @@ def downloadfile(ftp, remotepath, localpath, filename):
             filename = 'RETR ' + name  # 保存FTP文件
             ftp.retrbinary(filename, f.write)  # 保存FTP上的文件
             print("download " + name + " success")
+            result = True
     ftp.set_debuglevel(0)  # 关闭调试
     f.close()  # 关闭文件
+    return result
 
 # message:{"command":"pushModelpoints","data":"/home/ftpdir/models/yolov3.weights;N1334859111304531968","target":"N1334896073990213632;1234567890","timestamp":1607306166855}
 def pushModel(data):
@@ -42,10 +46,13 @@ def pushModel(data):
     localpath = "./model/"
     filename = sourceList[(len(sourceList)-1)]
     
+    result = False
     ftp = ftpconnect('47.94.44.231', "ftpdir", '123456')
-    downloadfile(ftp, remotepath, localpath, filename)
+    result = downloadfile(ftp, remotepath, localpath, filename)
     ftp.quit()
     print("=====================================")
+
+    return result
 
 if __name__ == "__main__":
     data = "/home/ftpdir/models/yolov3.weights;N1334859111304531968"
--- message_distribution.py
+++ message_distribution.py
@@ -1,38 +1,51 @@
- # -*- coding: utf-8 -*-
- # 接收到的消息分发
+# -*- coding: utf-8 -*-
+# 接收到的消息分发
 import json
 import time
-from pydash import get
+from pydash import get, set_
 from ftp_client import pushModel
+from mqtt_sender import sender
 
 # 消息例子
 # {"command":"pushModel;points","data":"/home/ftpdir/models/yolov3.weights;N1334859111304531968","target":["N1334896073990213632","1234567890"],"timestamp":1607944867}
 def distributor(msg):
     flag = True
-    msg_json =  json.loads( str(msg.payload.decode('utf-8')) )
-    
+    msg_json = json.loads(str(msg.payload.decode('utf-8')))
+
     # 排除之前的消息
     now_time = int(time.time())
-    get_time = int(get(msg_json, "timestamp", 0) )
-    if ( now_time - get_time ) >= 600 or ( now_time - get_time ) < 0:
+    get_time = int(get(msg_json, "timestamp", 0))
+    if (now_time - get_time) >= 600 or (now_time - get_time) < 0:
         flag = False
-    
+
     # 确定是自己的消息,编号为1234567890
     device_id = "1234567890"
     if (device_id not in get(msg_json, "target", [])) and (get(msg_json, "target", []) != "null"):
         flag = False
-    
+
     if flag:
         print("=====消息分发器=====")
         print(msg_json)
 
-        # 提取command
+        # 提取command,data
         command_split = get(msg_json, "command", []).split(";")
+        data_split = get(msg_json, "data", []).split(";")
+
         # 模型推送
         if get(command_split, '0', "") == "pushModel":
             # 指定推送
             if command_split[1] == "points":
-                pushModel(get(msg_json, "data", ""))
+                if pushModel(get(msg_json, "data", "")):
+                    send_data = {}
+
+                    set_(send_data, "deviceId", device_id)
+                    set_(send_data, "message", "modelUpdate") # type 说明是模型更新了
+                    set_(send_data, "data", get(data_split, "1", "")) # modelId
+                    set_(send_data, "timestamp", int(time.time()))
+
+                    # {"deviceId": "1234567890", "message": "modelUpdate", "data": "N1334859111304531968", "timestamp": 1607966540}
+                    sender(send_data)
             # 广播推送
             elif command_split[1] == "all":
-                pushModel(get(msg_json, "data", ""))
+                if pushModel(get(msg_json, "data", "")):
+                    i = 0
